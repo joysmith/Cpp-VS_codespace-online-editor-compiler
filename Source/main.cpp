5cf9f38fe58fb8bec1e7f781cf00036ef3571f18
joy smith peter <joysmith271@gmail.com>
2025-09-04 22:13:50 +0530

    r

---
+++ Source/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // statement terminated with semicolon
+    std::cout << "Hello, World!" << std::endl;
+
+    // tells OS that or program executed successfully
+    return 0;
+}
+
+
+/* output
+Hello, World!
+*/
